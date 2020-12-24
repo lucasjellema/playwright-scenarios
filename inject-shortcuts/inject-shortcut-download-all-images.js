970e7b83e868ed915a798d7642ea756b9d3728ba
Lucas Jellema <lucasjellema@gmail.com>
2020-12-24 15:11:30 +0100

    small refinements in shortcut for downloading all images

--- inject-shortcuts/inject-shortcut-download-all-images.js
+++ inject-shortcuts/inject-shortcut-download-all-images.js
@@ -14,7 +14,7 @@ var streamImageFromURL = function (imageURL, imageFilename) {
     request(imageURL).pipe(fs.createWriteStream(`${IMAGE_PATH}${imageFilename}`));
 }
 
-const shortCutExecutor = async (source) => {
+const allImageDownloader = async (source) => {
   console.log(`go download all images in the page`)
   // using the page object in source.page, get all img elements and return a collection of image objects to be processed in the Node context
   const images = await source.page.$$eval("img", (images) => 
@@ -29,6 +29,7 @@ const shortCutExecutor = async (source) => {
                               streamImageFromURL(image.src, imageFilename);
                             }                        
   });
+  return images.length
 }
 
 const snapshotter = async (source, text) => {
@@ -42,7 +43,7 @@ const snapshotter = async (source, text) => {
   const browser = await chromium.launch({ headless: false })
   const context = await browser.newContext()
   // expose a Node function as binding to the page (to be invoked from the function)
-  await context.exposeBinding('imageDownloadFunction', shortCutExecutor)
+  await context.exposeBinding('imageDownloadFunction', allImageDownloader)
   await context.exposeBinding('snapshotFunction', snapshotter)
   const page = await context.newPage();
 
@@ -50,8 +51,10 @@ const snapshotter = async (source, text) => {
   
   // create a shortcut key (ctrl + b) that triggers the JS function to download all images
   // create a shortcut key (ctrl + y) that triggers the JS function to take a snapshot of the page
-  const shortCutJS  = `function handleShortCutKey(e) { if ('KeyB'==e.code && e.ctrlKey) // ctrl + b
-                             window.imageDownloadFunction() ;  // invoke the Node function that was exposed to the browser context
+  const shortCutJS  = `async function handleShortCutKey(e) { if ('KeyB'==e.code && e.ctrlKey) { // ctrl + b
+                             const result = await window.imageDownloadFunction() ;  // invoke the Node function that was exposed to the browser context
+                             console.log(result+" images were downloaded")
+                         }
                          if ('KeyY'==e.code && e.ctrlKey) // ctrl + y
                              window.snapshotFunction() ;  // invoke the Node function that was exposed to the browser context
                        }
